9e688e0210d289d0fae5dd5dc196338650e9ebf0
marllonwagner <marllonwagner91@gmail.com>
2024-09-21 00:48:21 -0300

    starting...

---
+++ desafio-ultracar/src/main/java/com/ultracar/desafio_ultracar/DesafioUltracarApplication.java
@@ -0,0 +1,13 @@
+package com.ultracar.desafio_ultracar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUltracarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUltracarApplication.class, args);
+	}
+
+}
---
+++ desafio-ultracar/src/test/java/com/ultracar/desafio_ultracar/DesafioUltracarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultracar.desafio_ultracar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUltracarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
