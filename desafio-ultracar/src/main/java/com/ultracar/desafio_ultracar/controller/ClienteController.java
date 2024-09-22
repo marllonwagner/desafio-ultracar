20aea3f1777d37da1f7b9726174a4c5c40bb1e2f
marllonwagner <marllonwagner91@gmail.com>
2024-09-22 04:31:43 -0300

    controller comments add

--- desafio-ultracar/src/main/java/com/ultracar/desafio_ultracar/controller/ClienteController.java
+++ desafio-ultracar/src/main/java/com/ultracar/desafio_ultracar/controller/ClienteController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controlador responsável pela manipulação de clientes.
+ * Este controlador fornece endpoints para criar, listar, buscar,
+ * atualizar e deletar clientes.
+ */
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
@@ -23,31 +28,62 @@ public class ClienteController {
     this.clienteService = clienteService;
   }
 
+  /**
+   * Cria um novo cliente.
+   *
+   * @param clienteDto DTO contendo os dados do cliente a ser criado.
+   * @return ResponseEntity com o cliente criado e status HTTP 201 (Created).
+   */
   @PostMapping
   public ResponseEntity<Cliente> criarCliente(@Valid @RequestBody ClienteDTO clienteDto) {
     Cliente novoCliente = clienteService.salvarCliente(clienteDto);
     return ResponseEntity.status(HttpStatus.CREATED).body(novoCliente);
   }
 
+  /**
+   * Lista todos os clientes.
+   *
+   * @return ResponseEntity com a lista de clientes e status HTTP 200 (OK).
+   */
   @GetMapping
   public ResponseEntity<List<Cliente>> listarClientes() {
     List<Cliente> clientes = clienteService.listarClientes();
     return ResponseEntity.ok(clientes);
   }
 
+  /**
+   * Busca um cliente pelo seu ID.
+   *
+   * @param id ID do cliente a ser buscado.
+   * @return ResponseEntity com o cliente encontrado e status HTTP 200 (OK).
+   */
   @GetMapping("/{id}")
   public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Long id) {
     Cliente cliente = clienteService.buscarClientePorId(id);
     return ResponseEntity.ok(cliente);
   }
 
+  /**
+   * Atualiza um cliente existente.
+   *
+   * @param id ID do cliente a ser atualizado.
+   * @param clienteDto DTO contendo os novos dados do cliente.
+   * @return ResponseEntity com o cliente atualizado e status HTTP 200 (OK).
+   */
   @PutMapping("/{id}")
   public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id,
-    @Valid  @RequestBody ClienteDTO clienteDto) {
+      @Valid @RequestBody ClienteDTO clienteDto) {
     Cliente clienteAtualizado = clienteService.atualizarCliente(id, clienteDto);
     return ResponseEntity.ok(clienteAtualizado);
   }
 
+  /**
+   * Atualiza o endereço de um cliente existente.
+   *
+   * @param id ID do cliente cujo endereço deve ser atualizado.
+   * @param enderecoDto DTO contendo os novos dados do endereço.
+   * @return ResponseEntity com o cliente atualizado e status HTTP 200 (OK).
+   */
   @PutMapping("/{id}/endereco")
   public ResponseEntity<Cliente> atualizarEnderecoCliente(@PathVariable Long id,
       @RequestBody EnderecoDTO enderecoDto) {
@@ -55,6 +91,12 @@ public class ClienteController {
     return ResponseEntity.ok(enderecoAtualizado);
   }
 
+  /**
+   * Deleta um cliente pelo seu ID.
+   *
+   * @param id ID do cliente a ser deletado.
+   * @return ResponseEntity com status HTTP 204 (No Content) após a exclusão.
+   */
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
     clienteService.deletarCliente(id);
--- desafio-ultracar/src/main/java/com/ultracar/desafio_ultracar/controller/VeiculoController.java
+++ desafio-ultracar/src/main/java/com/ultracar/desafio_ultracar/controller/VeiculoController.java
@@ -11,6 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controlador responsável pela manipulação de veículos.
+ * Este controlador fornece endpoints para adicionar, atualizar e deletar veículos.
+ */
 @RestController
 @RequestMapping("/veiculos")
 public class VeiculoController {
@@ -22,19 +26,38 @@ public class VeiculoController {
     this.veiculoService = veiculoService;
   }
 
+  /**
+   * Adiciona um novo veículo.
+   *
+   * @param veiculoDto DTO contendo os dados do veículo a ser adicionado.
+   * @return ResponseEntity com a lista de veículos adicionados e status HTTP 201 (Created).
+   */
   @PostMapping
   public ResponseEntity<List<Veiculo>> addVeiculos(@Valid @RequestBody VeiculoDTO veiculoDto) {
     List<Veiculo> novosVeiculos = (List<Veiculo>) veiculoService.addVeiculo(veiculoDto);
     return ResponseEntity.status(HttpStatus.CREATED).body(novosVeiculos);
   }
 
+  /**
+   * Atualiza um veículo existente.
+   *
+   * @param id ID do veículo a ser atualizado.
+   * @param veiculoInfoDto DTO contendo os novos dados do veículo.
+   * @return ResponseEntity com o veículo atualizado e status HTTP 200 (OK).
+   */
   @PutMapping("/{id}")
   public ResponseEntity<Veiculo> updateVeiculo(@PathVariable Long id,
-    @Valid  @RequestBody VeiculoInfoDTO veiculoInfoDto) {
+      @Valid @RequestBody VeiculoInfoDTO veiculoInfoDto) {
     Veiculo veiculoAtualizado = veiculoService.updateVeiculo(id, veiculoInfoDto);
     return ResponseEntity.ok(veiculoAtualizado);
   }
 
+  /**
+   * Deleta um veículo pelo seu ID.
+   *
+   * @param id ID do veículo a ser deletado.
+   * @return ResponseEntity com status HTTP 204 (No Content) após a exclusão.
+   */
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteVeiculo(@PathVariable Long id) {
     veiculoService.deleteVeiculo(id);
